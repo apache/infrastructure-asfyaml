d3ffa64c109747885188f64a0e3dfcd8572fadfd
Daniel Gruno <humbedooh@apache.org>
2024-10-30 15:03:17 -0500

    not needed

--- feature/__init__.py
+++ feature/__init__.py
@@ -1,5 +1,4 @@
 # Simple init that gathers all the features and short-circuits ASFYamlFeature so it won't shadow itself inside
 # the features.
-from asfyaml import ASFYamlFeature
 
 from . import testfeature
