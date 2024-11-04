f385316eda12a2581e41a4f1b51f8bb5385b1872
Daniel Gruno <humbedooh@apache.org>
2024-11-04 15:30:46 -0600

    update unit tests

--- tests/validator.py
+++ tests/validator.py
@@ -13,6 +13,7 @@ if "tests" in os.getcwd():
     os.chdir("..")
 import pytest
 import asfyaml
+import repository
 asfyaml.DEBUG = True
 
 @pytest.mark.validator
@@ -20,11 +21,19 @@ def test_basic_yaml():
     EXPECTED_ENVS = {"production", "dev"}  # We expect these two envs enabled
     EXPECTED_FEATURES_MIN = {"test"}
     basic_yaml = open("tests/basic-dev-env.yaml", "r").read()
-    a = asfyaml.ASFYamlInstance(basic_yaml)
+    testrepo = repository.Repository("/x1/gitbox/repos/private/whimsy/whimsy-private.git")
+    a = asfyaml.ASFYamlInstance(testrepo, "humbedooh", basic_yaml)
     a.run_parts()
 
     # We should have both prod+dev envs enabled here
     assert a.environments_enabled == EXPECTED_ENVS
     # We should have at least our 'test' feature in the set
     assert EXPECTED_FEATURES_MIN.issubset(a.enabled_features.keys())
+
+    # The repo should be marked as private by asfyaml
+    assert testrepo.is_private is True, "Expected testrepo.private to be True, but wasn't!"
+
+    # Assert that we know the project name and the hostname
+    assert testrepo.project == "whimsy", f"Expected project name whimsy, but got {testrepo.project}"
+    assert testrepo.hostname == "whimsical", f"Expected project hostname whimsical, but got {testrepo.hostname}"
 
