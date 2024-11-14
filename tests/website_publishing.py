2ac34efdacb48c0650fa2f8fe5efa57b408b7602
Daniel Gruno <humbedooh@apache.org>
2024-11-14 10:51:11 -0600

    import YamlTest from helpers instead

--- tests/website_publishing.py
+++ tests/website_publishing.py
@@ -17,32 +17,9 @@ import asfyaml
 import dataobjects
 import contextlib
 import strictyaml
-
+from helpers import YamlTest
 # Set .asf.yaml to debug mode
 asfyaml.DEBUG = True
-
-
-class YamlTest:
-    def __init__(self, exc=None, errstr: str = None, yml=""):
-        self.exception = exc
-        self.errmsg = errstr
-        self.yaml = yml
-
-    @contextlib.contextmanager
-    def ctx(self):
-        if self.exception:
-            if self.errmsg:
-                my_ctx = pytest.raises(self.exception, match=self.errmsg)
-            else:
-                my_ctx = pytest.raises(self.exception)
-        else:
-            my_ctx = contextlib.nullcontext()
-        with my_ctx:
-            try:
-                yield self
-            finally:
-                pass
-
 
 valid_staging = YamlTest(
     None,
