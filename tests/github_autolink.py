b3dc6c0f7a3be99b2f1dac2e685c03d7a8ff02d3
Daniel Gruno <humbedooh@apache.org>
2024-11-14 10:53:11 -0600

    unit tests for autolink (jira) and features (issues, wiki, projects, discussions)

---
+++ tests/github_autolink.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+"""Unit tests for .asf.yaml github autolink features"""
+import os
+import sys
+from helpers import YamlTest
+sys.path.extend(
+    (
+        "./",
+        "../",
+    )
+)
+# If run locally inside the tests dir, we'll move one dir up for imports
+if "tests" in os.getcwd():
+    os.chdir("..")
+import pytest
+import asfyaml
+import dataobjects
+import contextlib
+import strictyaml
+
+# Set .asf.yaml to debug mode
+asfyaml.DEBUG = True
+
+
+valid_github_autolink = YamlTest(
+    None,
+    None,
+    """
+github:
+    autolink_jira:
+        - FOO
+        - BAR
+""",
+)
+
+valid_github_autolink_single = YamlTest(
+    None,
+    None,
+    """
+github:
+    autolink_jira: INFRA
+""",
+)
+
+# Something isn't uppercase alphabetical chars
+invalid_github_autolink_not_upperalpha = YamlTest(
+    strictyaml.exceptions.YAMLValidationError,
+    "String must be uppercase only",
+    """
+github:
+    autolink_jira:
+        - FOO
+        - bar
+""",
+)
+
+
+# not even a list!
+invalid_github_autolink_not_list = YamlTest(
+    strictyaml.exceptions.YAMLValidationError,
+    "when expecting a sequence",
+    """
+github:
+    autolink_jira: foo
+""",
+)
+
+
+def test_basic_yaml():
+    repo_path = "./repos/private/whimsy/whimsy-private.git"
+    os.environ["PATH_INFO"] = "whimsy-site.git/git-receive-pack"
+    os.environ["GIT_PROJECT_ROOT"] = "./repos/private"
+    if not os.path.isdir(repo_path):  # Make test repo dir
+        os.makedirs(repo_path, exist_ok=True)
+    testrepo = dataobjects.Repository(repo_path)
+
+    print("[github] Testing jira autolink features")
+
+    tests_to_run = (
+        valid_github_autolink,
+        valid_github_autolink_single,
+        invalid_github_autolink_not_upperalpha,
+        invalid_github_autolink_not_list
+    )
+
+    for test in tests_to_run:
+        with test.ctx() as vs:
+            a = asfyaml.ASFYamlInstance(testrepo, "humbedooh", test.yaml)
+            a.environments_enabled.add("noop")
+            a.no_cache = True
+            a.run_parts()
---
+++ tests/github_features.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""Unit tests for .asf.yaml github features"""
+import os
+import sys
+
+sys.path.extend(
+    (
+        "./",
+        "../",
+    )
+)
+# If run locally inside the tests dir, we'll move one dir up for imports
+if "tests" in os.getcwd():
+    os.chdir("..")
+import pytest
+import asfyaml
+import dataobjects
+import contextlib
+import strictyaml
+from helpers import YamlTest
+# Set .asf.yaml to debug mode
+asfyaml.DEBUG = True
+
+
+
+valid_github_features = YamlTest(
+    None,
+    None,
+    """
+github:
+    features:
+        issues: true
+        wiki: false
+        projects: true
+        discussions: false
+    labels:
+        - a
+        - b
+        - c
+""",
+)
+
+# Something isn't a bool
+invalid_github_features_not_bool = YamlTest(
+    strictyaml.exceptions.YAMLValidationError,
+    "expecting a boolean value",
+    """
+github:
+    features:
+        issues: maybe
+        wiki: false
+        projects: true
+        discussions: false
+""",
+)
+
+# Something isn't a valid directive
+invalid_github_features_unknown_directive = YamlTest(
+    strictyaml.exceptions.YAMLValidationError,
+    "unexpected key not in schema 'foobar'",
+    """
+github:
+    features:
+        foobar: true
+        wiki: false
+        projects: true
+        discussions: false
+""",
+)
+
+
+# Discussions enabled but no mailing list target set
+invalid_github_features_no_disc_target = YamlTest(
+    Exception,
+    "GitHub discussions can only be enabled if a mailing list target exists",
+    """
+github:
+    features:
+        issues: true
+        wiki: false
+        projects: true
+        discussions: true
+""",
+)
+
+
+def test_basic_yaml():
+    repo_path = "./repos/private/whimsy/whimsy-private.git"
+    os.environ["PATH_INFO"] = "whimsy-site.git/git-receive-pack"
+    os.environ["GIT_PROJECT_ROOT"] = "./repos/private"
+    if not os.path.isdir(repo_path):  # Make test repo dir
+        os.makedirs(repo_path, exist_ok=True)
+    testrepo = dataobjects.Repository(repo_path)
+
+    print("[github] Testing features")
+
+    tests_to_run = (
+        valid_github_features,
+        invalid_github_features_not_bool,
+        invalid_github_features_unknown_directive,
+        invalid_github_features_no_disc_target
+    )
+
+    for test in tests_to_run:
+        with test.ctx() as vs:
+            a = asfyaml.ASFYamlInstance(testrepo, "humbedooh", test.yaml)
+            a.environments_enabled.add("noop")
+            a.no_cache = True
+            a.run_parts()
