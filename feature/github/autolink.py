6e6d63d36f4a8d11c8bd431a93028aa417821674
Daniel Gruno <humbedooh@apache.org>
2024-11-14 17:46:17 -0600

    minor word tweaking

--- feature/github/autolink.py
+++ feature/github/autolink.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-"""GitHub autolink feature"""
+"""GitHub auto-link feature"""
 from . import directive, ASFGitHubFeature
 
 
 @directive
 def autolink(self: ASFGitHubFeature):
-    # Jira autolinking
-    autolink = self.yaml.get("autolink_jira")
-    if autolink:
+    # Jira auto-linking
+    autolink_jira = self.yaml.get("autolink_jira")
+    if autolink_jira:
         # If not a list, assume a string and listify it (we'll validate shortly...)
-        if not isinstance(autolink, list):
-            autolink = [autolink]
-        # Grab any existing autolinks (to ensure we don't recreate them over and over)
+        if not isinstance(autolink_jira, list):
+            autolink_jira = [autolink_jira]
+        # Grab any existing auto-links (to ensure we don't recreate them over and over)
         if not self.instance.no_cache:
             existing_autolinks = [x for x in self.ghrepo.get_autolinks()]  # Paginated (Iter) result -> list
         else:
             existing_autolinks = []
         # Now add the autolink if not already there
-        for jiraname in autolink:
-            jira_url = f"https://issues.apache.org/jira/browse/{jiraname}-<num>"
-            # Check whether the url_template matches an existing autolink. If not, create the autolink entry.
+        for jira_space in autolink_jira:
+            jira_url = f"https://issues.apache.org/jira/browse/{jira_space}-<num>"
+            # Check whether the url_template matches an existing auto-link. If not, create the auto-link entry.
             if not any(jira_url == al.url_template for al in existing_autolinks):
-                print(f"Setting up new auto-link for {jiraname}-<num> -> {jira_url}")
+                print(f"Setting up new auto-link for {jira_space}-<num> -> {jira_url}")
                 if not self.noop("autolink_jira"):
-                    self.ghrepo.create_autolink(key_prefix=f"{jiraname}-", url_template=jira_url)
+                    self.ghrepo.create_autolink(key_prefix=f"{jira_space}-", url_template=jira_url)
