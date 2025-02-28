9a4b205703830f51c6bc5f70630deb400bb12278
Thomas Neidhart <tn@apache.org>
2025-02-28 16:55:17 +0100

    fix indentation

--- asfyaml/feature/github/housekeeping.py
+++ asfyaml/feature/github/housekeeping.py
@@ -22,7 +22,7 @@ from . import directive, ASFGitHubFeature
 def housekeeping_features(self: ASFGitHubFeature):
     del_branch_on_merge = self.yaml.get("del_branch_on_merge", None)
     if del_branch_on_merge is not None and not self.noop("del_branch_on_merge"):
-            self.ghrepo.edit(delete_branch_on_merge=del_branch_on_merge)
+        self.ghrepo.edit(delete_branch_on_merge=del_branch_on_merge)
 
     dependabot_alerts = self.yaml.get("dependabot_alerts", None)
     if dependabot_alerts is not None and not self.noop("dependabot_alerts"):
