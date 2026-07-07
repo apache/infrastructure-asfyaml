b46f2a81314399a501164e939c388e4477314198
Daniel Gruno <humbedooh@apache.org>
2026-07-07 22:59:03 +0200

    lint fix: newline

--- asfyaml/feature/notifications.py
+++ asfyaml/feature/notifications.py
@@ -66,6 +66,7 @@ VALID_PRIVATE_TARGETS = [
 # regex for valid ASF mailing list
 RE_VALID_MAILING_LIST = re.compile(r"[-a-z0-9]+@(?:[-a-z0-9]+\.)?(?:(?:incubator\.)?apache\.org|apachecon\.com)$")
 
+
 class ASFNotificationsFeature(ASFYamlFeature, name="notifications", priority=0):
     """.asf.yaml notifications feature class. Runs before anything else."""
 
